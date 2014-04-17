08f1808843de7d6a8dfa66084a20c89802b64445
Filippos Lymperopoulos <filippos.lymperopoulos@students.olin.edu>
2014-04-17 02:47:49 -0400

    Added intersecting map method.

--- HackingCrypto.py
+++ HackingCrypto.py
@@ -22,3 +22,17 @@ def addLettersToMapping(letterMapping,cipherword,candidate):
 		if candidate[i] not in letterMapping[cipherword[i]]:
 			letterMapping[cipherword[i]].append(candidate[i])
 	return letterMapping
+
+def intersectMappings(Map1,Map2):
+	""" Takes the intersection of the two dictionaries maps generated, to produce the most universal map. """
+	Map3 = getBlankCipherletterMapping()
+	for letter in alphabet:
+		if Map1[letter] == []:
+			Map3[letter] = copy.deepcopy(Map2[letter])
+		elif Map2[letter] == []:
+			Map3[letter] = copy.deepcopy(Map1[letter])
+		else:
+			for i in Map1[letter]:
+				if i in Map2[letter]:
+					Map3[letter].append(i)
+	return Map3
