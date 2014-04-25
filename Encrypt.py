38d8db7b94dacdf6bca1eb133f6c156dfe66a279
Haley Pelletier <haley.pelletier@students.olin.edu>
2014-04-24 23:04:32 -0400

    Added docstrings and comments

--- Encrypt.py
+++ Encrypt.py
@@ -10,6 +10,7 @@ alpha_dict = dict((alpha_list[i],i) for i in range(26))
 
 
 def split_message(message,n):
+	"""Splits the plaintext message into blocks of n letters"""
 	message = message.lower()
 	message = message.replace(" ","")
 	blocks = []
@@ -19,6 +20,7 @@ def split_message(message,n):
 
 
 def create_key(n):
+	"""Randomly generates an n-by-n matrix (numbers 0-25) to use as a key"""
 	key = []
 	for i in range(n):
 		line = []
@@ -27,38 +29,39 @@ def create_key(n):
 			line.append(rand)
 		key.append(line)	
 	det = int(round(numpy.linalg.det(key)))%26	
-	if det != 0 and det%2 != 0 and det%13 != 0:
+	if det != 0 and det%2 != 0 and det%13 != 0:   #Makes sure the determinant isn't 0 or divisible by 2 or 13
 		return key
 	else:		
 		return create_key(n)
 
 
 def encrypt_block(block,key):
+	"""Encrypts each block by multiplying it by the key"""
 	letters = list(block)
 	
 	num_block = []
-	for i in range (len(letters)):		
+	for i in range (len(letters)):		         #Converts the block to numbers
 		num_block.append(alpha_dict[letters[i]])
 		
 	key_block = [0]*len(num_block)	
 	for i in range (len(key)):
-		for j in range (len(key[i])):
+		for j in range (len(key[i])):            #Multiplies the block by the key (mod 26)
 			key_block[i] += key[i][j] * num_block[j]	
 		key_block[i] = key_block[i]%26
 	
 	ciph_block = ['']*len(num_block)	
-	for i in range (len(key_block)):
+	for i in range (len(key_block)):             #Converts the block to ciphertext letters
 		ciph_block[i] = alpha_list[key_block[i]]
 	return''.join(ciph_block)
 
 
 def Encrypt_Message(message,n):
+	"""Encrypts a given message using the Hill Cipher
+		Inputs: message to be encrypted, and n (size of the key matrix)
+		Outputs: ciphertext and key in a dictionary"""
 	blocks = split_message(message,n)
 	key = create_key(n)
 	ciph_blocks = []
 	for block in blocks:				
-		ciph_blocks.append(encrypt_block(block,key))	
-	return {'ciphertext':''.join(ciph_blocks),'key':key}
-
-
-#print Encrypt_Message('multiples of n',3)
+		ciph_blocks.append(encrypt_block(block,key))  #Joins the encrypted blocks
+	return {'ciphertext':''.join(ciph_blocks),'key':key}  #Returns the ciphertext and the key as a dictionary
--- Hill_Cipher.py
+++ Hill_Cipher.py
@@ -1,24 +1,21 @@
-import random
-import string
-import numpy
-from fractions import Fraction
 from numpy import *
 import ModInverse
 import Encrypt
 import Decrypt
 
-message = 'KIRYUIN SATSUKIIIIIIII'
-n = 3
 
-vals = Encrypt.Encrypt_Message(message,n)
+plaintext = 'multiples of n'  #Your plaintext message to be encrypted (must be divisible by n)
+n = 3                         #Size of the encryption matrix/key
+
+vals = Encrypt.Encrypt_Message(plaintext,n)
 ciphertext = vals['ciphertext']
 key = vals['key']
 inv_key = ModInverse.modular_inverse(key)
 inv_key = inv_key.tolist()
 return_message = Decrypt.Decrypt_Message(ciphertext,inv_key)
 
-print matrix(key)
-print matrix(inv_key)
-print message
-print ciphertext
-print return_message
+print 'Key: \n' + str(matrix(key))
+print 'Inverse Key: \n' +  str(matrix(inv_key))
+print 'Plaintext: ' + plaintext
+print 'Ciphertext: ' + ciphertext
+print 'Output: ' + return_message
