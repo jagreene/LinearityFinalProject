1a54d90961fdb7ac1aa19f0084105e6b53afdf49
Filippos Lymperopoulos <filippos.lymperopoulos@students.olin.edu>
2014-04-27 22:56:36 -0400

    Added ceaser encryption and decryption for txt files and constitution.

--- FinalCeaser.py
+++ FinalCeaser.py
@@ -2,22 +2,30 @@
 
 import numpy as np
 
-text = 'hey'
+text = open('constitution.txt','r')
 
-textVec = np.array([ord(x) for x in text])
 
-shift = 258
+textVec = np.array([[ord(i) for i in x] for x in text.readlines()]) 
+textVec = np.hstack(textVec.flat)
+# textVec = textVec.flatten()
+
+# print textVec
+shift = 1
 
 shiftVec = shift*np.ones(len(textVec))
+# print type(textVec[0])
+# print type(shiftVec[0])
 
 ciphertext = shiftVec + textVec
 
 cipherText = np.array([chr(int(x)%256) for x in ciphertext])
 
-print cipherText
+# print ''.join(cipherText)
 
 ciphertext -= shiftVec
 
 cipherText = np.array([chr(int(x)%256) for x in ciphertext])
 
-print cipherText
+# print ''.join(cipherText)
+
+print ord('')
